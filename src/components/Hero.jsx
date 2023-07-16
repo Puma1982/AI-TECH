34e2ea0d8a2afc3de66068052db71b040c7e8232
VIP-HACKER <griss_hai2005@hotmail.com>
2023-07-16 13:41:24 +0200

    Logo Raduis done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        <img src={logo}  className='w-28 rounded-full object-contain' />
 
         <button
   type='button'
